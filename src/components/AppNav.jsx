c4e10a45965c3f2aa327d6708ffdbba93cb5a16e
swasty jay <amekpoagbedaniel@gmail.com>
2025-01-18 21:25:03 +0000

    added all components

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,6 +1,11 @@
 import styles from "./AppNav.module.css";
 
 const AppNav = () => {
-  return <nav className={styles.nav}>App Navigation</nav>;
+  return (
+    <nav className={styles.nav}>
+      App Navigation
+      <p className={styles.nav}>swasty jay</p>
+    </nav>
+  );
 };
 export default AppNav;
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,25 +1,27 @@
 import { NavLink } from "react-router-dom";
-import styles from "./NavPage.module.css";
+import Logo from "./Logo";
+import styles from "./PageNav.module.css";
 
-const NavPage = () => {
+function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
+
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
         </li>
-        {/* <li>
-          <NavLink to="/">Home</NavLink>
-        </li> */}
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
-};
+}
 
-export default NavPage;
+export default PageNav;
