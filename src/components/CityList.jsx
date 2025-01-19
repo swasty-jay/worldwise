63e2b9e6aadb89ed222068259fc20e124eaa31c0
swasty jay <amekpoagbedaniel@gmail.com>
2025-01-19 01:17:55 +0000

    added country item

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,8 +1,13 @@
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
+import Message from "./Message";
 const CityList = ({ cities, isloading }) => {
   if (isloading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
